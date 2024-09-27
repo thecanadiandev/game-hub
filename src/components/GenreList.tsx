ce9bdbf06a8f5eb521584977401bae2d6bfbd76e
Hari <harikrishkk@gmail.com>
2024-09-27 01:10:04 -0400

    game hub repo

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,60 @@
+import {
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import { getCroppedImageUrl } from "../services/image-url";
+
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
+}
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+  const { data: genres, isLoading, error } = useGenres();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (error) {
+    return null;
+  }
+  return (
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                fontSize="lg"
+                variant="link"
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default GenreList;
