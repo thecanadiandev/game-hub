3a98b49acd5d4496b57fa42cc284d99f7836bf96
Hari <harikrishkk@gmail.com>
2024-09-29 13:06:52 -0400

    add routing + fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -21,6 +21,9 @@ class APIClient<T> {
     return axiosInstance.get<FetchResponse<T>>(this.endpoint, config)
       .then(res => res.data)
   }
+  get = (id: string | number) => {
+    return axiosInstance.get<T>(this.endpoint + '/' + id).then(res => res.data)
+  }
 }
 
 export default APIClient;
