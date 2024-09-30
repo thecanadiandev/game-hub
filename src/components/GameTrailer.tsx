62a6b750f094f646918d46157ebcb63880e4fedc
Hari <harikrishkk@gmail.com>
2024-09-30 18:17:05 -0400

    add game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,18 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  const first = data?.results[0];
+
+  if (error || isLoading) {
+    return null;
+  }
+  return first ? (
+    <video poster={first.preview} src={first.data[480]} controls></video>
+  ) : null;
+};
+
+export default GameTrailer;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -6,6 +6,7 @@ import ExpandableText from "../components/ExpandableText";
 import DefinitionItem from "../components/DefinitionItem";
 import CriticScore from "../components/CriticScore";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -22,6 +23,8 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText> {game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
